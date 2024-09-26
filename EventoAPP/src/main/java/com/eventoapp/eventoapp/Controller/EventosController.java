b270b4507e36d288e3faea23e170e6aa9b9b7d26
KaynanSouza <kaynan.souza@sempreceub.com>
2024-09-26 20:56:56 -0300

    add

--- EventoAPP/src/main/java/com/eventoapp/eventoapp/Controller/EventosController.java
+++ EventoAPP/src/main/java/com/eventoapp/eventoapp/Controller/EventosController.java
@@ -3,6 +3,7 @@ package com.eventoapp.eventoapp.Controller;
 import com.eventoapp.eventoapp.models.Evento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.eventoapp.eventoapp.repository.EventoRepository;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,4 +31,20 @@ public class EventosController {
         modelAndView.addObject("eventos", eventos);
         return modelAndView;
     }
+
+    @RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
+    public ModelAndView detalhesEvento(@PathVariable("codigo") long id){
+        Evento evento = er.findById(id);
+        ModelAndView modelAndView = new ModelAndView("detalhesEvento");
+        modelAndView.addObject("evento", evento);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/deletar")
+    public String deletarEvento(long id){
+        Evento evento = er.findById(id);
+        er.delete(evento);
+        return "redirect:/eventos";
+    }
+
 }
